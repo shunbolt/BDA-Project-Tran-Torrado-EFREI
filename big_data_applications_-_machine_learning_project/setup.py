7d4cb53f065028f74d1be2301aa39a2e431e1166
Raphaël Tran <raphaeltrann@hotmail.com>
2020-10-14 15:53:03 +0200

    Added cookiecutter template

---
+++ big_data_applications_-_machine_learning_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Big Data Applications - ML project for EFREI - 2021',
+    author='Rapahaël Tran - Yoann Torrado',
+    license='',
+)
