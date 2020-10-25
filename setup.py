a9b93d5000966e5a0617211503c85d9607f34778
Raphaël Tran <raphaeltrann@hotmail.com>
2020-10-25 16:57:02 +0100

    Added corrections + gitignore update

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     packages=find_packages(),
     version='0.1.0',
     description='Big Data Applications - ML project for EFREI - 2021',
-    author='Rapahaël Tran - Yoann Torrado',
+    author='Raphaël Tran - Yoann Torrado',
     license='',
 )
